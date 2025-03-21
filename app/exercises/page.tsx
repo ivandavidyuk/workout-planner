6b85d396f78f86549f329a4b073e2e302b9f8191
vanyadavidyuk <vanyadavidyuk@yandex.ru>
2025-03-21 15:30:43 +0300

    linter error fix #3

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { EXERCISES } from "../data/exercises";
 
@@ -8,10 +10,10 @@ const ExercisesPage = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {EXERCISES.map((exercise) => (
           <Link key={exercise.id} href={`/exercises/${exercise.id}`}>
-            <a className="border rounded p-4 flex items-center space-x-4 hover:bg-gray-100">
+            <div className="border rounded p-4 flex items-center space-x-4 hover:bg-gray-100">
               <img src={exercise.image} alt={exercise.name} className="w-16 h-16 object-cover rounded" />
               <span className="text-lg font-medium">{exercise.name}</span>
-            </a>
+            </div>
           </Link>
         ))}
       </div>
